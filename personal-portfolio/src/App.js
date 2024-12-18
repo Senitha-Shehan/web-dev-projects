f726b042eebbc6d7a44ba33a61d0acc83842bb85
Senitha-Shehan <senitha200109@gmail.com>
2024-12-18 22:04:15 +0530

    Skills Commit

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'; // Update the path if it's in the components folder.
 import Banner from './components/Banner';
+import Skiils from './components/Skiils';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skiils />
     </div>
   );
 }
