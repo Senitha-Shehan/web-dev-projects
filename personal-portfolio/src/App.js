d8959a1b15bdf49cbccdcc26630b280df3e916ce
Senitha-Shehan <senitha200109@gmail.com>
2024-12-18 12:52:21 +0530

    delete navbar

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/NavBar';
-
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <h1>Personal Portfolio</h1>
     </div>
   );
 }
