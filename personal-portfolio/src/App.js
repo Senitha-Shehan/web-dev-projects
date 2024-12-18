fe3a31364a0b35a34645d55fcdeb91fae9899cf9
Senitha-Shehan <senitha200109@gmail.com>
2024-12-18 22:43:12 +0530

    Projects Commit
    Done Project UI

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'; // Update the path if it's in the components folder.
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
