ef4d93d589a90bf6b69d7828d2bc0a1682f16d18
Senitha-Shehan <senitha200109@gmail.com>
2024-12-18 13:55:29 +0530

    Commit Banner
    Done Banner Section

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'; // Update the path if it's in the components folder.
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
