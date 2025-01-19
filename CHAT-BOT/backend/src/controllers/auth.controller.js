4e6a47f6bd9cba960dae7bc22fcef75aa87506af
Senitha-Shehan <senitha200109@gmail.com>
2025-01-19 13:48:12 +0530

    User Model

--- CHAT-BOT/backend/src/controllers/auth.controller.js
+++ CHAT-BOT/backend/src/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 export const singup = (req, res) => {
-    res.send("Singup Route");
-}
+  res.send("Singup Route");
+};
 
 export const login = (req, res) => {
-    res.send("Login Route");
-}
+  res.send("Login Route");
+};
 
 export const logout = (req, res) => {
-    res.send("Logout Route");
-}
+  res.send("Logout Route");
+};
--- CHAT-BOT/backend/src/index.js
+++ CHAT-BOT/backend/src/index.js
@@ -14,4 +14,3 @@ app.listen(PORT, () => {
   console.log("Server Running PORT " + PORT);
   connectdb();
 });
-
