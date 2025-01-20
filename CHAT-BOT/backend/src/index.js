76901f257b7b4e16b41b3ad843a52af7423fec4c
Senitha-Shehan <senitha200109@gmail.com>
2025-01-20 11:16:03 +0530

    message controller

--- CHAT-BOT/backend/src/index.js
+++ CHAT-BOT/backend/src/index.js
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 import { connectdb } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.json());
-
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT;
 
