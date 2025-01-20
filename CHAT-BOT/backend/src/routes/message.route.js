fd03701ae47f39058456f1cc20646a966dc8e974
Senitha-Shehan <senitha200109@gmail.com>
2025-01-20 11:31:35 +0530

    end backend setup

--- CHAT-BOT/backend/src/routes/message.route.js
+++ CHAT-BOT/backend/src/routes/message.route.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
 import { getUsersForSidebar } from "../controllers/message.controller.js";
 import { getMessages } from "../controllers/message.controller.js";  
 import { sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 
-router.get("/users",protectRoute, getUsersForSidebar);
-router.get("/:id",protectRoute, getMessages);
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute,sendMessage);
 
