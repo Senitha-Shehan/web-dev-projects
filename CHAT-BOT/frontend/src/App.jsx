8e424c019c6337591d536159deb7448faba08a19
Senitha-Shehan <senitha200109@gmail.com>
2025-02-04 13:33:03 +0530

    Update App.jsx

--- CHAT-BOT/frontend/src/App.jsx
+++ CHAT-BOT/frontend/src/App.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import { Routes, Route, Navigate } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
+
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
-import Footer from "./components/Footer";
-import useThemeStore from "./store/useThemeStore";
 
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
-function App() {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore(); // Destructure isCheckingAuth
-
-  const { theme } = useThemeStore()
+  console.log({ onlineUsers });
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log(authUser);
+  console.log({ authUser });
 
-  if (isCheckingAuth)
+  if (isCheckingAuth && !authUser)
     return (
-      // Show loading spinner if checking auth
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
       </div>
@@ -57,10 +58,7 @@ function App() {
       </Routes>
 
       <Toaster />
-
-      <Footer />
     </div>
   );
-}
-
+};
 export default App;
